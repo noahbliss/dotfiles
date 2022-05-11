7ec16bd78802ab84553c2ceb9da72f5c9e222a9e
Noah Bliss <noah@superuser.sh>
2022-05-11 17:46:18 +0000

    Create quality_of_life.sh

---
+++ quality_of_life.sh
@@ -0,0 +1,2 @@
+# Librewolf linked to Firefox for gnome extension integration (works on Manjaro at least):
+sudo ln -s /usr/lib/mozilla/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
